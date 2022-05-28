8c6dece1cb2655ce8640fff991ce6881b9ce0fa3
RalfMog <104436602+RalfMog@users.noreply.github.com>
2022-05-28 20:34:24 +0300

    Update UserServiceImpl.java

--- src/main/java/web/service/UserServiceImpl.java
+++ src/main/java/web/service/UserServiceImpl.java
@@ -10,38 +10,34 @@ import web.entity.User;
 import java.util.List;
 
 @Service
-
+@Transactional
 public class UserServiceImpl implements UserService {
 
-    private UserDao userDao;
+    private final UserDao userDao;
 
     @Autowired
     public UserServiceImpl(UserDao userDao) {
         this.userDao = userDao;
     }
 
-    @Transactional
     public void saveUser(User user) {
         userDao.saveUser(user);
     }
 
-    @Transactional
     public User updateUser(User user) {
         return userDao.updateUser(user);
     }
 
-    @Transactional
     public User findById(int id) {
         return userDao.findById(id);
     }
 
-    @Transactional
     public void deleteById(int id) {
         userDao.deleteById(id);
     }
 
-    @Transactional
     public List<User> findAll() {
         return userDao.findAll();
     }
 }
+
